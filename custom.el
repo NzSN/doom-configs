0444b88bdded43b045ee041cfa75d16ac53ce8a4
NzSN <aydenlin@foxmail.com>
2022-01-10 12:39:00 +0800

    init

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-solarized-dark))
+ '(custom-safe-themes
+   '("cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
+ '(org-agenda-files '("~/Notes/Brain/20211226110640-sche_2022_01.org"))
+ '(package-selected-packages '(ggtags)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-themes 'disabled nil)
