08488bda800e5c1cf285e2c9bedb1f44d1125071
NzSN <aydenlin@foxmail.com>
2022-01-10 23:30:56 +0800

    Add org-roam-ui

--- custom.el
+++ custom.el
@@ -3,19 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-solarized-dark))
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes '(doom-solarized-dark-high-contrast))
  '(custom-safe-themes
-   '("cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
+   '("f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
  '(lsp-headerline-breadcrumb-enable t)
  '(org-agenda-files '("~/Notes/Brain/20211226110640-sche_2022_01.org"))
  '(org-agenda-skip-scheduled-if-deadline-is-shown nil)
  '(org-agenda-span 30)
- '(package-selected-packages '(ccls ggtags)))
+ '(package-selected-packages '(ccls ggtags))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Ubuntu Mono derivative Powerline" :foundry "DAMA" :slant normal :weight normal :height 218 :width normal)))))
 (put 'customize-themes 'disabled nil)
 (put 'customize-variable 'disabled nil)
