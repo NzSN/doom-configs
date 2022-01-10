5e80eb4c595482f2bab0fab4fbf29a0d0fc384eb
NzSN <aydenlin@foxmail.com>
2022-01-11 00:38:08 +0800

    Add go, coq

--- custom.el
+++ custom.el
@@ -8,6 +8,17 @@
  '(custom-enabled-themes '(doom-solarized-dark-high-contrast))
  '(custom-safe-themes
    '("f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
+ '(default-frame-alist
+    '((right-divider-width . 1)
+      (bottom-divider-width . 1)
+      (tool-bar-lines . 0)
+      (vertical-scroll-bars)
+      (menu-bar-lines . 0)
+      (buffer-predicate . doom-buffer-frame-predicate)
+      (height . 55)
+      (width . 80)
+      (left-fringe . 4)
+      (right-fringe . 4)))
  '(lsp-headerline-breadcrumb-enable t)
  '(org-agenda-files '("~/Notes/Brain/20211226110640-sche_2022_01.org"))
  '(org-agenda-skip-scheduled-if-deadline-is-shown nil)
