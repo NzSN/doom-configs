a7a9b46528a4a26a1bb9c2c0aca0ac4b66e74f16
NzSN <aydenlin@foxmail.com>
2022-02-17 10:20:45 +0800

    update

--- config.el
+++ config.el
@@ -63,7 +63,6 @@
 ;; GPG key to use for encryption
 ;; Either the Key ID or set to nil to use symmetric encryption.
 (setq org-crypt-key nil)
-(setq epa-file-select-keys t)
 
 ;; Org-roam v2 configures
 (use-package! websocket
