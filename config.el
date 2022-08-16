e59bc185740897eb2c43f731ab003457e3b40701
NzSN <aydenlin@foxmail.com>
2022-08-16 14:06:09 +0800

    Add chinese support to org-latex

--- config.el
+++ config.el
@@ -112,3 +112,14 @@
   (setq company-show-numbers t)
   (setq company-idle-delay 0)
 )
+
+;; org-mode latex chinese support
+(with-eval-after-load 'ox-latex
+  (add-to-list 'org-latex-classes
+               '("ctexart" "\\documentclass[11pt]{ctexart}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (setq org-latex-compiler "xelatex"))
