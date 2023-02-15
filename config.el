5652846d390f3ee43d85d29669977e175509461e
NzSN <aydenlin@foxmail.com>
2023-02-15 17:29:32 +0800

    add tracking logging to todo keywords

--- config.el
+++ config.el
@@ -147,3 +147,10 @@
 ;;(add-hook 'js2-mode-hook 'deno-fmt-mode)
 
 (setq lsp-use-plists "true")
+
+(require 'org)
+(setq org-todo-keywords
+    '((sequence "TODO(t!)" "PROJ(p)" "LOOP(r)" "STRT(s!)" "WAIT(w!)" "HOLD(h!)" "IDEA(i)" "|" "DONE(d!)" "KILL(k!)")
+      (sequence "[ ](T!)" "[-](S)" "[?](W)" "|" "[X](D!)")
+      (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
+
