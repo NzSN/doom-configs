b3ee5c0b24ca922125cdbb63ac49fba4096fce9f
Linshizhi <linshizhi@laihua.com>
2023-02-24 08:12:42 +0800

    todo keywords tracking edit

--- config.el
+++ config.el
@@ -151,6 +151,6 @@
 (require 'org)
 (setq org-todo-keywords
     '((sequence "TODO(t)" "PROJ(p)" "LOOP(r)" "STRT(s!)" "WAIT(w!)" "HOLD(h!)" "IDEA(i)" "|" "DONE(d!)" "KILL(k!)")
-      (sequence "[ ](T!)" "[-](S)" "[?](W)" "|" "[X](D!)")
+      (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D!)")
       (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
 
