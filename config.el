0a12e11bb38d832a7a9e71501fb313307b0f0899
NzSN <aydenlin@foxmail.com>
2023-07-11 22:23:56 +0800

    Use clangd as lsp client by default

--- config.el
+++ config.el
@@ -156,6 +156,15 @@
          (split-string-and-unquote path ":")
          exec-path)))
 
+;; lsp mode
+(setq lsp-clients-clangd-args '("-j=3"
+				"--background-index"
+				"--clang-tidy"
+				"--completion-style=detailed"
+				"--header-insertion=never"
+				"--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
 ;; (map! "C-c C" #'centaur-tabs--kill-this-buffer-dont-ask)
 
 ;; Copilot
