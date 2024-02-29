f6fbac561940a537073455f073a8fc557c2a7899
NzSN <nzsn0101@gmail.com>
2024-03-01 00:04:13 +0800

    Enable lsp for swift-mode

--- config.el
+++ config.el
@@ -222,3 +222,6 @@
   :after lsp-mode
   :config
   (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
+
+(use-package swift-mode
+  :hook (swift-mode . (lambda () (lsp))))
