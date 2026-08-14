5c2b3684e4640b11a33ea21d6fe3909fa6a34dff
NzSN <nzsn0101@gmail.com>
2026-08-14 17:20:41 +0800

    enable minimap

--- init.el
+++ init.el
@@ -49,14 +49,14 @@
        :ui
        ;;deft                         ; notational velocity for Emacs
        doom                           ; what makes DOOM look the way it does
-       dashboard                 ; a nifty splash screen for Emacs
+       dashboard                      ; a nifty splash screen for Emacs
        doom-quit                      ; DOOM quit-message prompts when you quit Emacs
        (emoji +unicode)               ; 🙂
        hl-todo                        ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;;indent-guides                  ; highlighted indent columns
+       ;;indent-guides                ; highlighted indent columns
        ;;(ligatures +extra)           ; ligatures and symbols to make your code pretty again
-       ;;minimap                      ; show a map of the code on the side
+       minimap                        ; show a map of the code on the side
        modeline                       ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash                    ; blink cursor line after big motions
        ;;neotree                      ; a project drawer, like NERDTree for vim
